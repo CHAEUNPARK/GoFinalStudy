ae597f8dbf0392515702211c9bf084c3a7561c96
CHAEUN PARK <codjs0910@naver.com>
2020-01-10 11:27:54 +0900

    struct A B

--- src/practice0109/practice0109.go
+++ src/practice0109/practice0109.go
@@ -5,8 +5,19 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type SectionA struct {
+	A int
+	B int
+}
+
+type SectionB struct {
+	C int
+	D int
+}
 
 func Contains(bs []byte, b byte) bool {
 	for _, value := range bs {
@@ -25,6 +36,7 @@ func Find(bs []byte, b byte) int {
 	}
 	return len(bs)
 }
+
 func main() {
 	fo, err := os.Open("src/practice0109/config.conf")
 	if err != nil {
@@ -35,7 +47,11 @@ func main() {
 	var whitespace byte = ' '
 	var sBracketF byte = '['
 	var sBracketB byte = ']'
+	var sectionName string
+	secA := new(SectionA)
+	secB := new(SectionB)
 	reader := bufio.NewReader(fo)
+
 	for {
 		line, isPrefix, err := reader.ReadLine()
 		if isPrefix {
@@ -48,27 +64,70 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-
+		if len(line) == 0{
+			continue
+		}
 		if check := Find(line, sBracketF); check == 0 {
 			if check := Contains(line, whitespace); check {
 				fmt.Println("Syntax Error : 공백이 들어가 있습니다.")
-				break
+				return
 			}
 			if check := Find(line, sBracketB); check != len(line)-1 {
 				fmt.Println("Syntax Error : ] 가 없습니다.")
-				break
+				return
 			}
-			sectionName := string(line[1 : len(line)-1])
-			fmt.Println(sectionName)
+			sectionName = string(line[1 : len(line)-1])
 		} else {
 			if check := Contains(line, whitespace); check {
 				fmt.Println("Syntax Error : 공백이 들어가 있습니다.")
-				break
+				return
 			}
-			key := strings.Split(string(line), "=")[0]
-			value := strings.Split(string(line), "=")[1]
-			fmt.Println(key, value)
+			if sectionName == "SectionA" {
+				if key := strings.Split(string(line), "=")[0]; key == "A" {
+					value, err := strconv.Atoi(strings.Split(string(line), "=")[1])
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					secA.A = value
+				} else if key := strings.Split(string(line), "=")[0]; key == "B" {
+					value, err := strconv.Atoi(strings.Split(string(line), "=")[1])
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					secA.B = value
+				} else {
+					fmt.Println("Invalid Section Name")
+					return
+				}
+			} else if sectionName == "SectionB" {
+				if key := strings.Split(string(line), "=")[0]; key == "C" {
+					value, err := strconv.Atoi(strings.Split(string(line), "=")[1])
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					secB.C = value
+				} else if key := strings.Split(string(line), "=")[0]; key == "D" {
+					value, err := strconv.Atoi(strings.Split(string(line), "=")[1])
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+					secB.D = value
+				} else {
+					fmt.Println("Invalid Section Name")
+					return
+				}
+			}
 		}
-
 	}
+	fmt.Println("SectionA")
+	fmt.Println("A : ", secA.A)
+	fmt.Println("B : ", secA.B)
+
+	fmt.Println("SectionB")
+	fmt.Println("C : ", secB.C)
+	fmt.Println("D : ", secB.D)
 }
