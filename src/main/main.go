ba5c03570a6e40c07f9d03e28db9059616b49f1a
CHAEUN PARK <codjs0910@naver.com>
2020-01-20 17:31:39 +0900

    logrus test

--- src/main/main.go
+++ src/main/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"MyLog"
+	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
-	logger := MyLog.MyLog{}
-	logger.Init("src/logger/", "logger.log", "INFO")
-	for i := 0 ; i < 10 ; i++{
-		logger.WriteLogTrace(i, "hihi", i+1)
+	log := logrus.New()
+	log.SetLevel(logrus.InfoLevel)
+	log.SetFormatter(&logrus.TextFormatter{})
+	log.SetReportCaller(true)
+
+	fo, err := os.OpenFile(
+		"src/main/logrus.log",
+		os.O_APPEND|os.O_CREATE,
+		644,
+		)
+	if err != nil{
+		log.Fatalln("file open fail")
 	}
+	log.SetOutput(fo)
+	defer fo.Close()
+
+	log.Infoln("infolevel")
 
 }
