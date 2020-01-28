1519eb847f1f31d815a80bbb999322e646467dac
CHAEUN PARK <codjs0910@naver.com>
2020-01-28 13:37:31 +0900

    echo framework practice

---
+++ src/echoprac/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
+	"time"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	e.POST("/save", save)
+	e.GET("/writeCookie", writeCookie)
+	e.GET("/readCookie", readCookie)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team : "+team+", member : "+member)
+}
+
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name : "+name+"email : "+email)
+}
+
+func writeCookie(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "username"
+	cookie.Value = "jon"
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	c.SetCookie(cookie)
+	return c.String(http.StatusOK, "write a cookie")
+}
+
+func readCookie(c echo.Context) error {
+	cookie, err := c.Cookie("username")
+	if err != nil {
+		return err
+	}
+	fmt.Println(cookie.Name)
+	fmt.Println(cookie.Value)
+	return c.String(http.StatusOK, "read a cookie")
+}
